515daad3d75738142fd3b652a1226c236f7e55d9
tomi <remioluwatomicephas@gmail.com>
2026-04-22 07:17:59 +0100

    feat: implemented http server with per process and per request hashes + \nmulti-stage docker build

--- log-output/main.go
+++ log-output/main.go
@@ -1,15 +1,57 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	// "fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
-func main() {
-	ticker := time.NewTicker(5000 * time.Millisecond)
-	defer ticker.Stop()
+func generateHash() string {
+	key := make([]byte, 32)
 
-	for t := range ticker.C {
-		fmt.Println(t)
+	rand.Read(key)
+
+	return base64.StdEncoding.EncodeToString(key)
+}
+
+var hashPort int
+
+func init() {
+	flag.IntVar(&hashPort, "port", 8081, "port which the server runs on")
+
+	flag.Parse()
+}
+
+func main() {
+	processHash := generateHash()
+
+	type RequestResponse struct {
+		ApplicationHash string `json:"appHash"`
+		RequestHash     string `json:"reqHash"`
 	}
+
+	hashHandler := func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		hashRes := RequestResponse{
+			ApplicationHash: processHash,
+			RequestHash:     generateHash(),
+		}
+
+		err := json.NewEncoder(w).Encode(hashRes)
+		if err != nil {
+			//handle marshalling error
+			http.Error(w, "failed to encode response", http.StatusInternalServerError)
+			return
+		}
+
+	}
+
+	http.HandleFunc("/hash", hashHandler)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(hashPort), nil))
+
 }
